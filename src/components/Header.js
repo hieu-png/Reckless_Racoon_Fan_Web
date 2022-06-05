0563ca5a807f91dd2ad62b92747c18bcef8f681f
taiyounari@gmail.com <49590699+taiyounari@users.noreply.github.com>
2022-06-05 22:09:04 +0700

    Icon change

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 import { AppBar, Box, Toolbar, Typography, Button, Link, IconButton, useScrollTrigger, Slide } from "@mui/material";
 
 
-import discord_big_outline from "../assets/icon/discord_big_outline.svg";
-import twitter_big_outline from "../assets/icon/twitter_big_outline.svg";
-import instagram_big_outline from "../assets/icon/instagram_big_outline.svg";
-import { discordInviteCode, twitterHandle, instagramName, backgroundColor, projectName } from "../constant";
+import { ReactComponent as DiscordIcon } from "../assets/icon/discord_big_outline.svg";
+import { ReactComponent as TwitterIcon } from "../assets/icon/twitter_big_outline.svg";
+import { ReactComponent as InstagramIcon } from "../assets/icon/instagram_big_outline.svg";
+import { discordInviteCode, twitterHandle, instagramName, backgroundColor, projectName, primaryColor } from "../constant";
 
 function HideOnScroll(props) {
     const { children, window } = props;
@@ -105,21 +105,23 @@ export default function Header(props) {
                         <Button href={`https://discord.com/invite/${discordInviteCode}`}
                             target="_blank" rel="noopener noreferrer"
                             color="inherit">
-                            <img src={discord_big_outline} alt="join discord server" />
+                            <DiscordIcon fill={backgroundColor} stroke={primaryColor} stroke-width='0'/>
                         </Button>}
 
                     {twitterHandle !== "" &&
                         <Button href={`https://twitter.com/${twitterHandle}`}
                             target="_blank" rel="noopener noreferrer"
                             color="inherit">
-                            <img src={twitter_big_outline} alt="open twitter Page" />
+                            <TwitterIcon fill={backgroundColor} stroke={primaryColor} stroke-width='0' />
+
                         </Button>}
 
                     {instagramName !== "" &&
                         <Button href={`https://www.instagram.com/${instagramName}`}
                             target="_blank" rel="noopener noreferrer"
                             color="inherit">
-                            <img src={instagram_big_outline} alt="open instagram page" />
+                            <InstagramIcon fill={backgroundColor} stroke={primaryColor} stroke-width='0' />
+
                         </Button>}
                 </Toolbar>
             </AppBar>
